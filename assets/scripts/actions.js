2920bdaca4299a917bfab2c51216c0781262b0f8
Tolga ÇINAR <cinarrtolga@gmail.com>
2021-02-05 11:11:04 +0000

    Description box addd to top-right corner

--- assets/scripts/actions.js
+++ assets/scripts/actions.js
@@ -1,3 +1,12 @@
+$(document).ready(function () {
+    $(".description-show-button").click(function () {
+        toggleDescriptionContent(false);
+    });
+    $(".description-close-button").click(function() {
+        toggleDescriptionContent(true);
+    });
+});
+
 //After loading, this method changes visibility. 
 //Time out to create a soft transition.    
 function getCanvas() {
@@ -14,10 +23,25 @@ function getModal(selectedPart, mouseX, mouseY) {
     $("#dialog").dialog({
         modal: true,
         show: 'fade',
-        open: function(){
+        open: function () {
             $(".ui-dialog").css("top", mouseY);
             $(".ui-dialog").css("left", mouseX);
             $(".ui-dialog-content").css("min-height", "auto");
         }
     });
+}
+
+//This function for show / hide description content. Function decides to act according to status.
+function toggleDescriptionContent(isActive) {
+    if (isActive) {
+        $(".description-show-button").addClass("load");
+        $(".description-show-button").removeClass("unload");
+        $(".content-description-area").addClass("unload");
+        $(".content-description-area").removeClass("load");
+    } else {
+        $(".description-show-button").removeClass("load");
+        $(".description-show-button").addClass("unload");
+        $(".content-description-area").removeClass("unload");
+        $(".content-description-area").addClass("load");
+    }
 }
